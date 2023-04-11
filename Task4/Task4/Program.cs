1099175a0324f4bfbe9ac02e91c81c8f13f0e084
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-04-11 23:41:07 +0300

    task4

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your number : ");
+            string number = Console.ReadLine();
+            string result = "";
+
+            for (int i = number.Length; i > 0; i--)
+            {
+                if (i % 3 == 0 && i != number.Length)
+                {
+                    result += ",";
+                }
+                result += number[number.Length - i];
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
