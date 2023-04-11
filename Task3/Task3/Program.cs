0389c8af334b27d0af8698b3091a07561fe2ebe9
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-04-11 23:18:25 +0300

    NumberSum

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Enter your number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Number Sum => " + NumberSum(number));
+        }
+        public static int NumberSum(int number)
+        {
+            int NumberSum = 0;
+            while (true)
+            {
+                if (number > 0)
+                {
+                    NumberSum += number % 10;
+                    number = number / 10;
+                }
+                else
+                {
+                    return NumberSum;
+                }
+
+            }
+        }
+    }
+}
