291c7064938447e1785153125668462990dd9adb
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-04-11 09:22:31 +0300

    Too-Bar

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your number=> ");
+            int number = int.Parse(Console.ReadLine());
+            for(int i = 0; i < number; i++) 
+            { 
+                if(i % 3 == 0) 
+                {
+                    Console.Write(i);
+                    Console.WriteLine("too");
+                }
+                else if (i % 5 == 0) 
+                {   
+                    Console.Write(i);
+                    Console.WriteLine(" Bar");                
+                }
+                else if(i % 3 == 0 && i % 5 == 0) 
+                {
+                    Console.Write(i);
+                    Console.WriteLine("too bar");
+                }
+            }
+            
+        }
+    }
+}
