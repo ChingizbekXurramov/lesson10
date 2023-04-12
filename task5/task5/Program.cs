4e7c4a8ba5e7a5c9353096266ec3e47a7140f666
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-04-12 09:34:45 +0300

    n dan m gacha toq sonlar

---
+++ task5/task5/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Gotonumber:
+            Console.Write("enter first number==> ");
+            int number = int .Parse(Console.ReadLine());
+            Console.Write("Enter second number==> ");
+            int number2 = int .Parse(Console.ReadLine());
+            
+            if (number < number2)
+            {
+                for(int i = number; i < number2; i++) 
+                {
+                    if (number % 2 != 0)
+                    {
+                        Console.Write(i + ",");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine( "Please enter > number"); 
+                goto Gotonumber;
+                
+            }
+         
+        }
+    }
+}
